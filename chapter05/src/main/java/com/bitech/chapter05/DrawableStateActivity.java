3558c91fc10ee51d321292a39836b9a1d39a683f
LDD1WHU <dandan.lin@bitech-automotive.com>
2024-10-21 15:53:46 +0800

    状态列表图形

---
+++ chapter05/src/main/java/com/bitech/chapter05/DrawableStateActivity.java
@@ -0,0 +1,19 @@
+package com.bitech.chapter05;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DrawableStateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_drawable_state);
+    }
+}
