3113f3028630d84c0fc3e44901e719c327204b4c
LDD1WHU <dandan.lin@bitech-automotive.com>
2024-11-01 14:28:02 +0800

    存储卡上读取图片文件

--- chapter06/src/main/java/com/bitech/chapter06/enity/FileUtil.java
+++ chapter06/src/main/java/com/bitech/chapter06/enity/FileUtil.java
@@ -1,7 +1,12 @@
 package com.bitech.chapter06.enity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,13 +19,12 @@ import java.nio.Buffer;
  **/
 public class FileUtil {
 
-    //把字符串保存到指定路径的文本文件
+    // 把字符串保存到指定路径的文本文件
     public static void saveText(String path, String txt) {
         BufferedWriter os = null;
         try {
             os = new BufferedWriter(new FileWriter(path));
             os.write(txt);
-
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -34,10 +38,9 @@ public class FileUtil {
         }
     }
 
-    //从指定路径的文本文件中读取内容字符串
-
+    // 从指定路径的文本文件中读取内容字符串
     public static String openText(String path) {
-        BufferedReader is=null;
+        BufferedReader is = null;
         StringBuilder sb = new StringBuilder();
         try {
             is = new BufferedReader(new FileReader(path));
@@ -47,7 +50,7 @@ public class FileUtil {
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (is != null) {
                 try {
                     is.close();
@@ -57,5 +60,46 @@ public class FileUtil {
             }
         }
         return sb.toString();
+    }
+
+    // 把位图数据保存到指定路径的图片文件
+    public static void saveImage(String path, Bitmap bitmap) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(path);
+            // 把位图数据压缩到文件输出流中
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 从指定路径的图片文件中读取位图数据
+    public static Bitmap openImage(String path) {
+        Bitmap bitmap = null;
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(path);
+            bitmap = BitmapFactory.decodeStream(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bitmap;
     }
 }
