6d6cc885ab1c969ab985194d54892bd9df0135c4
LDD1WHU <dandan.lin@bitech-automotive.com>
2024-11-04 15:04:19 +0800

    生命周期

--- chapter06/src/main/java/com/bitech/chapter06/MyApplication.java
+++ chapter06/src/main/java/com/bitech/chapter06/MyApplication.java
@@ -12,30 +12,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.HashMap;
+
 public class MyApplication extends Application {
+
+    private static MyApplication mApp;
+    // 声明一个公共的信息映射对象，可当作全局变量使用
+    public HashMap<String, String> infoMap = new HashMap<>();
+
+
+
+    public static MyApplication getInstance() {
+        return mApp;
+    }
 
 
     //在App启动时调用
     @Override
     public void onCreate() {
         super.onCreate();
+        mApp = this;
         Log.d("ldd", "MyApplication onCreate");
+
     }
 
 
-
-    //在APP终止时调用
+    //在App终止时调用
     @Override
     public void onTerminate() {
         super.onTerminate();
-        Log.d("ldd", "onTerminate");
+        Log.d("ning", "onTerminate");
     }
 
-    //在配置改变时调用，列如从竖屏变为横屏
-
+    //在配置改变时调用，例如从竖屏变为横屏。
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        Log.d("ldd", "onConfigurationChanged");
+        Log.d("ning", "onConfigurationChanged");
     }
+
 }
